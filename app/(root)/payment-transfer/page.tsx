aa86d3910d9d4c5fe37ef257e5261012cd0d3522
Samuel <soccersha120@gmail.com>
2026-02-15 18:54:21 +0800

    Implementation on displaying data bank, recent transaction, connecting multiple bank accounts,  transaction history page, my banks page, transfer payment page, displaying real time data, pagination, and refactoring

--- app/(root)/payment-transfer/page.tsx
+++ app/(root)/payment-transfer/page.tsx
@@ -6,17 +6,17 @@ import React from 'react'
 
 const Transfer = async () => {
   const loggedIn = await getLoggedInUser();
-  const accounts = await getAccounts({ 
-    userId: loggedIn.$id 
+  const accounts = await getAccounts({
+    userId: loggedIn.$id
   })
 
-  if(!accounts) return;
-  
+  if (!accounts) return;
+
   const accountsData = accounts?.data;
 
   return (
     <section className="payment-transfer">
-      <HeaderBox 
+      <HeaderBox
         title="Payment Transfer"
         subtext="Please provide any specific details or notes related to the payment transfer"
       />
